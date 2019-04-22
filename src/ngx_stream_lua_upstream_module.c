b4669951f58caadf4687f8c6d1e244f2a3afb082
Yigitcan Basalma <Yigitcan.Basalma@n11.com>
2019-04-22 19:48:09 +0300

    Working on ngx_stream_lua_get_request

--- src/ngx_stream_lua_upstream_module.c
+++ src/ngx_stream_lua_upstream_module.c
@@ -22,7 +22,7 @@ static ngx_int_t ngx_stream_lua_upstream_init(ngx_conf_t *cf);
 static int ngx_stream_lua_upstream_create_module(lua_State * L);
 static int ngx_stream_lua_upstream_get_upstreams(lua_State * L);
 static int ngx_stream_lua_upstream_get_servers(lua_State * L);
-static ngx_stream_upstream_main_conf_t *
+static ngx_stream_lua_main_conf_t *
     ngx_stream_lua_upstream_get_upstream_main_conf(lua_State *L);
 static int ngx_stream_lua_upstream_get_primary_peers(lua_State * L);
 static int ngx_stream_lua_upstream_get_backup_peers(lua_State * L);
@@ -480,7 +480,7 @@ ngx_stream_lua_get_peer(lua_State *L, ngx_stream_upstream_rr_peer_t *peer,
 }
 
 
-static ngx_stream_upstream_main_conf_t *
+static ngx_stream_lua_main_conf_t *
 ngx_stream_lua_upstream_get_upstream_main_conf(ngx_conf_t *cf)
 {
     ngx_stream_lua_main_conf_t       *lmcf;
